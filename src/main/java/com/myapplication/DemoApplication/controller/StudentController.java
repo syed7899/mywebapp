44e3f9125917372f279d87e642472844c277401a
syed <talentinfo9999@gmail.com>
2023-07-07 21:32:24 +0530

    3 methods

--- src/main/java/com/myapplication/DemoApplication/controller/StudentController.java
+++ src/main/java/com/myapplication/DemoApplication/controller/StudentController.java
@@ -1,4 +1,20 @@
 package com.myapplication.DemoApplication.controller;
 
 public class StudentController {
+
+
+
+
+
+    public void subraction(){
+
+    }
+
+    public void multiplication(){
+
+    }
+
+    public void division(){
+
+    }
 }
